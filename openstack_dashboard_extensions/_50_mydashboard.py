9ff259e41fd268f78a47b62529256917bdc1e7fd
Rakesh <rakesh.podishetty@calsoftinc.com>
2017-04-11 17:38:01 +0530

    code changes done

--- openstack_dashboard_extensions/_50_mydashboard.py
+++ openstack_dashboard_extensions/_50_mydashboard.py
@@ -1,5 +1,5 @@
 # The slug of the dashboard to be added to HORIZON['dashboards']. Required.
-DASHBOARD = 'mydashboard_openstack_dashboard'
+DASHBOARD = 'mydashboard'
 
 # A list of applications to be added to INSTALLED_APPS.
 ADD_INSTALLED_APPS = [
